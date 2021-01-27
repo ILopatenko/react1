96bc81d7b806af77da2728890ce03c90f9be4c45
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-26 21:36:51 -0800

    Add props. TC 02:20

--- src/index.js
+++ src/index.js
@@ -2,42 +2,37 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
+//Setup all the variables
+const author = 'Napoleon Hill';
+const title = 'Think and Grow Rich: The Landmark Bestseller Now Revised and Updated for the 21st Century (Think and Grow Rich Series)';
+const img = 'https://images-na.ssl-images-amazon.com/images/I/61y04z8SKEL._SX349_BO1,204,203,200_.jpg';
+
+
 //This is a main component
 const Booklist = () => { 
     return (
     <section className="booklist">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book job="developer"/>
+        <Book name="Ron" title="hello"/>
     </section>
     );
 };
 
-const Book = () => {
+
+
+
+const Book = (props) => {
+    console.log(props);
     return <article className="book">
-        <Image />
-        <Title />
-        <Author />
+       <img src={img} alt=""/>
+       <h3>{author.toUpperCase()}</h3>
+       <h4>{title}</h4>
+       {console.log(props.job)}
+
+       <p>{props.job}</p>
+       <p>{props.title}</p>
     </article>;
 };
-
-const Image = () => {
-    return <img src="https://images-na.ssl-images-amazon.com/images/I/61y04z8SKEL._SX349_BO1,204,203,200_.jpg" alt=""/>
-};
-
-const Author = () => <h3>Napoleon Hill</h3>
-
-const Title = () => <h4>Think and Grow Rich: The Landmark Bestseller Now Revised and Updated for the 21st Century (Think and Grow Rich Series)</h4>
-
-
-
-
-
-
 
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
