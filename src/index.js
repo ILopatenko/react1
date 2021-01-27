91fa5a176c90ba92f8625197e3aff61fb57e5ab1
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-26 19:57:22 -0800

    Add 2 components (arrow function and regular function). TC 01:30

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const MyModule = () => { return <h4>MyConponent v.0</h4> };
+//An example of arrow function wich returns a block <h4> with some text inside another block <div> using JSX
+const MyModule1 = () => { 
+    return <div><h4>MyConponent v.0</h4></div>
+};
 
-ReactDom.render(<MyModule />, document.getElementById('root'));
+//An example of regular function wich returns a block <h4> with some text using createElement functoin of React
+function MyModule2() {
+    return React.createElement('h4', {}, 'Hello from non-arrow function');
+};
+
+ReactDom.render(<MyModule1 />, document.getElementById('root'));
+ReactDom.render(<MyModule2 />, document.getElementById('root2'));
+
