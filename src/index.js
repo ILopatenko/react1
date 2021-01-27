a4871843440cc13038ff0cbcf0f295958eed282f
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-26 20:06:42 -0800

    React will render a main module. Main module consists of 2 differen module. All the modules are in the index.js file. TC 01:33

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//An example of arrow function wich returns a block <h4> with some text inside another block <div> using JSX
-const MyModule1 = () => { 
-    return <div><h4>MyConponent v.0</h4></div>
+//This is a main component
+const MyAppGeneralModule = () => { 
+    return (
+    <div>
+        <MyModule1 />
+        <MyModule2 />
+
+    </div>
+    );
 };
 
 //An example of regular function wich returns a block <h4> with some text using createElement functoin of React
-function MyModule2() {
+function MyModule1() {
     return React.createElement('h4', {}, 'Hello from non-arrow function');
 };
 
-ReactDom.render(<MyModule1 />, document.getElementById('root'));
-ReactDom.render(<MyModule2 />, document.getElementById('root2'));
+const MyModule2 = () => { 
+    return (
+    <div>
+        <h4>MyConponent v.0</h4>
+    </div>
+    );
+};
+
+ReactDom.render(<MyAppGeneralModule />, document.getElementById('root'));
+
 
