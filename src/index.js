77674e031a094c5a8dbef33cf07deb46ac4c3176
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-27 18:44:49 -0800

    Example of using props with objects as arguments. TC 02:26

--- src/index.js
+++ src/index.js
@@ -2,38 +2,36 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
-//Setup all the variables
-const author = 'Napoleon Hill';
-const title = 'Think and Grow Rich: The Landmark Bestseller Now Revised and Updated for the 21st Century (Think and Grow Rich Series)';
-const img = 'https://images-na.ssl-images-amazon.com/images/I/61y04z8SKEL._SX349_BO1,204,203,200_.jpg';
+//Setup all the variables for a book as an object
+const firstBook = {
+    author: 'Irene E. Schoenberg, Jay Maurer',
+    title: 'Focus on Grammar, Vol. 1: An Integrated Skills Approach, 2nd Edition',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51OrgRz7MHL._SX381_BO1,204,203,200_.jpg',
+};
 
+const secondBook = {
+    author: 'Workman Publishing, Michael Geisen, Sharon Madanes',
+    title: 'Everything You Need to Ace Science in One Big Fat Notebook: The Complete Middle School Study Guide (Big Fat Notebooks)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51+Db2crJOL._SX359_BO1,204,203,200_.jpg',
+};
 
 //This is a main component
 const Booklist = () => { 
     return (
     <section className="booklist">
-        <Book job="developer"/>
-        <Book name="Ron" title="hello"/>
+        <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+        <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
     </section>
     );
 };
 
-
-
-
 const Book = (props) => {
-    console.log(props);
     return <article className="book">
-       <img src={img} alt=""/>
-       <h3>{author.toUpperCase()}</h3>
-       <h4>{title}</h4>
-       {console.log(props.job)}
-
-       <p>{props.job}</p>
-       <p>{props.title}</p>
+       <img src={props.img} alt=""/>
+       <h2>{props.author.toUpperCase()}</h2>
+       <h4>{props.title}</h4>
     </article>;
 };
-
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
 
