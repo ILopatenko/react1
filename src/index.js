6a71634cbbccac904ca712e8fb36d5220016d173
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 15:00:35 -0800

    Add onClick events. TC03:09

--- src/index.js
+++ src/index.js
@@ -41,10 +41,12 @@ function BookList() {
 //Setup Component Book - it will return a description for each book
 const Book = (props) => {
 
-
+    const complexExample = () => {
+        console.log(author);
+    };
 
     //Create a handler for an event
-    const clickHandler = () => {
+    const clickHandler = (author) => {
         alert(`you have pushed the button at book: ${author}!`);
     };
 
@@ -55,7 +57,12 @@ const Book = (props) => {
         <img src={img} alt=""/>
         <h2 onClick={() => console.log(`You have toched: ${title}`)}>{author}</h2>
         <h4>{title}</h4>
-        <button type="button" onClick={clickHandler}>example</button>
+        <button type="button" onClick={clickHandler}> example </button>
+
+        <button type="button" onClick={complexExample(author)}> onClick event with handeler </button>
+
+        <button type="button" onClick={() => {complexExample(author)}}> onClick event with arrow function wich run another function  </button>
+
         </article>
     );
 };
