56405096b53b8a278dcda3b95dae902b746b06e8
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 20:27:51 -0800

    Use a single property states with an object. TC04:26:30

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
   const [person, setPerson] = useState({name: "Peter", age: 25, message: "WTF?!?"});
-  console.log(person);
+
   const newMessage = 'Hey whatsup?!?';
+
+  const [name, setName] = useState('Peter');
+  const [age, setage] = useState(25);
+  const [message, setMessage] = useState('hey guys!');
+
   const changeMessage = () => {
-    setPerson({...person, message : newMessage})
+    setMessage('hello world!!!')
   };
   return <>
-    <h3>Persone name is: {person.name}.</h3>
-    <h3>Persone name is: {person.age}.</h3>
-    <h3>Persone message is: {person.message}.</h3>
+    <h3>Persone name is: {name}.</h3>
+    <h3>Persone name is: {age}.</h3>
+    <h3>Persone message is: {message}.</h3>
 
-    <button className="btn" onClick={changeMessage}>Change persone message to {newMessage}</button>
+    <button className="btn" onClick={changeMessage}>Change persone message </button>
   </>
 };
 
