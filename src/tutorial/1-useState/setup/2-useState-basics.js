e2f1450cc096f899d6a1c64d38d9fac141a86528
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 19:01:37 -0800

    Basic functionality for useState hook. Create a button wich can change a text. TC04:01

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  /* console.log(useState('ae!'));
+
+  const value = useState('f..ck you!')[0];
+  console.log(value);
+
+  const handler = useState('f..ck you!')[1];
+  console.log(value, handler); */
+
+
+  const initialTitle = 'initial title as an example';
+  const newTitle = 'ABSOLUTELY NEW TITLE';
+  const [text, setText] = useState(initialTitle);
+  const handleClick = () => {
+    text !== initialTitle ? setText(initialTitle) : setText(newTitle);
+  };
+  
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}> Let's try to change the title</button>
+    </React.Fragment>
 };
 
 export default UseStateBasics;
