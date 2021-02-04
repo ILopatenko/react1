3cfbf32d5cfa0d16dd3642b03b7ec384fe37b720
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 18:07:43 -0800

    Start a new empty project for advanced REACT part. TC03:32

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-
-//Import data from other places/files
-import {books} from "./books";
-import { Book } from "./Book";
-import { greeting } from "../src/testing/testing";
-
-//This is a main component
-function BookList() { 
-    console.log(greeting);
-    return (
-    <section className="booklist">
-        {books.map((book) => {
-            return (
-                <Book key={book.id} {...book}></Book>
-            );
-        })} 
-    </section>
-    );
-};
-
-//Render our main component BookList to div with id='root' in index.html
-ReactDom.render(<BookList />, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
