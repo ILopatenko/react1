b9380dc8a207c7378d978eeefeba0fbb0538c758
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 20:03:45 -0800

    Use useState with array (create a button to delete all the items from an array, create a buttons to delete each item separately). TC04:17

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,30 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  //Set up an initial state as an array
+  const [people, setPeople] = React.useState(data);
+
+  return <>
+    {
+      people.map(person => {
+        const {id, name} = person;
+        const removeItem = id => {
+          let newPeople = people.filter(person => person.id !== id)
+          setPeople(newPeople);
+        }
+        return <div key={id} className="item">
+  
+          <h4>{name}</h4>
+          <button className="btn" onClick={() => {removeItem(id)}}> Delete!  </button>
+ 
+        </div>
+      })
+
+    }
+    <button className="btn" onClick={() => setPeople([])}> {people.length === 0 ? 'All the items were already deleted' : 'CLEAR ALL THE ITEMS'} </button>
+  </>
 };
 
 export default UseStateArray;
+ 
