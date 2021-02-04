f986abfc2a210c1fe44837a85a8f4ba48126f6b4
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 20:45:19 -0800

    Create a regular counter with using a useState hook. Increase and Decrease work with outer functions, Reset works with inner function. TC04:32

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
+import { Fragment } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(5);
+  const decrease = () => {setValue(value - 1)};
+  const increase = () => {setValue (value + 1)};
+  return <Fragment>
+    <section style={{margin: '4rem 0'}}>
+      <h2>regular counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={increase}>+</button>
+      <button className='btn' onClick={() => {setValue(0)}}>RESET</button>
+      <button className='btn' onClick={decrease}>-</button>
+    </section>
+  </Fragment>
 };
 
 export default UseStateCounter;
