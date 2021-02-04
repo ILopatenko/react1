8118cc2ed0ae730b8894cb7e4c6b79a1acdf47f7
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-03 20:20:30 -0800

    useState with object. Change a value with a button. TC04:23

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({name: "Peter", age: 25, message: "WTF?!?"});
+  console.log(person);
+  const newMessage = 'Hey whatsup?!?';
+  const changeMessage = () => {
+    setPerson({...person, message : newMessage})
+  };
+  return <>
+    <h3>Persone name is: {person.name}.</h3>
+    <h3>Persone name is: {person.age}.</h3>
+    <h3>Persone message is: {person.message}.</h3>
+
+    <button className="btn" onClick={changeMessage}>Change persone message to {newMessage}</button>
+  </>
 };
 
 export default UseStateObject;
