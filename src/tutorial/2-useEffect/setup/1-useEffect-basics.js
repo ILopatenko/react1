79cbc577b7da2ed328cca2ce0e23a26bf823e05b
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-06 14:21:56 -0800

    Button changes document title. TC04:58

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { Fragment } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+  const [value, setValue] = useState(0); 
+
+  useEffect(() => {
+    console.log('call useEffect!');
+    document.title = `You have ${value} new messages!`;
+  });
+
+  console.log('render component');
+
+  return <Fragment>
+    <h1>{value}</h1>
+    <button className='btn' onClick={() => setValue(value+1)}>Click me fast!</button>
+  </Fragment>
 };
 
 export default UseEffectBasics;
