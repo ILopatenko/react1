3ca5750e0c84b624497a22c78b00e83d7ed3ae6c
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-07 12:31:51 -0800

    Multiple returns. Fetch the data from github. TC052:52

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -4,11 +4,47 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
+  useEffect(()=>{
+    setIsLoading(true)
+    fetch(url)
+    .then((resp)=>{
+      if(resp.status >= 200 && resp.status <= 299){
+        return resp.json()
+      } else {
+        setIsLoading(false)
+        setIsError(true)
+        throw new Error (resp.statusText)
+      }
+    })
+    .then((user)=>{
+      const {login} = user;
+      setUser(login);
+      setIsLoading(false);
+      
+    })
+    .catch(error => console.log(error))
+  },[]);
+
+  if (isLoading){
+    return <>
+      <div>Loading ...</div>
+    </>
+  };
+
+  if (isError){
+    return <>
+      <div>Error ...</div>
+    </>
+  };
   
   return <>
-  <button type='button' className='btn' onClick={()=>setLoading(!loading)}>change loading</button>
-    {loading === true ? <h2>Current loading is TRUE</h2> : <h2>Current loading is FALSE</h2>}
+    <div>
+      <h1>{user}</h1>
+    </div>
   </>
   
 };
