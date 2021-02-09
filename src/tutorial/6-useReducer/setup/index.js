64e5defa8c69b5f8775618312e2daaa39c41b8ab
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-08 19:43:53 -0800

    Basic functionality prepared for REDUSER. TC07:30

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,44 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName('');
+    } else {
+      setShowModal(true);
+    }
+  };
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit' className='button'>
+          Add an item to list
+        </button>
+      </form>
+      {people.map((el) => {
+        return (
+          <div key={el.id}>
+            <h4>{el.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
