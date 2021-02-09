cdeed37c2b8a2e82242591a78b24bcd780af1047
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-08 18:59:14 -0800

    useRef() hook. TC07:16

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,33 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input type='text' ref={refContainer} />
+          <button type='button' onClick={handleSubmit}>
+            submit
+          </button>
+        </div>
+      </form>
+      <div ref={divContainer}>hello my friend</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
