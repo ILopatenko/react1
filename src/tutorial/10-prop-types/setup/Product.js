ec985c343af8043243032662ea7c1123b4422a97
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-10 17:24:11 -0800

    PropTypes and default values. TC08:59

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defIm from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ id, name, price, image }) => {
+  const isUrl = image && image.url;
+  return (
+    <article className='product'>
+      {/*Custom check
+      {image ? <img src={image.url} alt={name} /> : 'No image'} 
+      */}
+      <img src={isUrl || defIm} alt={name || 'NaMe'} />
+
+      <h4>{name}</h4>
+
+      <p>{id}</p>
+
+      {/*Custom check
+      <p>{price ? `price is: $${price}` : 'Price upon request!'}</p> 
+      */}
+
+      <p>${price || 999}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+/* Product.defaultProps = {
+  image: defIm,
+  name: 'Name is missing ...',
+  price: '$-0',
+  id: 'ID is missing ...',
+}; */
 
 export default Product;
